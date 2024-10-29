779c5feb3a4c1b1b2cef9d7ce73c60945acea54d
startexport <start.export@icloud.com>
2024-10-29 05:44:04 +0100

    Initial commit of GuessingGame project

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int numberToGuess = random.Next(1, 101);
+        int userGuess = 0;
+        int numberOfGuesses = 0;
+
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        Console.WriteLine("I'm thinking of a number between 1 and 100.");
+        Console.WriteLine("Can you guess what it is?");
+
+        while (userGuess != numberToGuess)
+        {
+            Console.Write("Enter your guess: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out userGuess))
+            {
+                numberOfGuesses++;
+
+                if (userGuess > numberToGuess)
+                {
+                    Console.WriteLine("Too high! Try again.");
+                }
+                else if (userGuess < numberToGuess)
+                {
+                    Console.WriteLine("Too low! Try again.");
+                }
+                else
+                {
+                    Console.WriteLine($"Congratulations! You guessed the number in {numberOfGuesses} tries.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+    }
+}
